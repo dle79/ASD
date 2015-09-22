799000088c3b3d1e86f8969c675606b6dca41d60
nnkhuong <nguyenngockhuong@gmail.com>
2015-09-21 19:33:04 -0500

    update

--- FiCo/src/framework/model/AccountFactory.java
+++ FiCo/src/framework/model/AccountFactory.java
@@ -15,23 +15,11 @@ public class AccountFactory implements IAccountFactory {
 		return null;
 	}
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 69ec0837f038fe89e351711f1028a8534c5097b9
 	@Override
 	public ICustomer createCustomer(String name, CustomerType customerType,
 			IAddress add, String email, String bdate) {
 	    CustomerFactory cf = new CustomerFactory();
 	    return cf.createCustomer(name, customerType, add, email, bdate);
-		
 	}
-<<<<<<< HEAD
-
-
-	
- 
-=======
->>>>>>> 69ec0837f038fe89e351711f1028a8534c5097b9
 
 }
